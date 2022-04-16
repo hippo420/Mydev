6389a4f4df4f9c306a17a4f0134bf339e4a08dc0
gaebabja <hippo420@naver.com>
2022-04-16 17:20:31 +0900

    상수 2차정리, 텍스트데이터저장 및 파일쓰기

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Constants {
+public class Constants {
+
+    //FORM 관련
+    final String FORM = "set_name";
+    final String FORM_NAME ="set_titletext";
+    final String FORM_SIZE ="_setFormPosition";
+
+    //Dataset
+    final String DataSet_NAME="new Dataset";
+    final String DataSet_CONTENTS="_setContents";
+    //추가영역
+    final String ADDCHILD = "addchild";
+    //UI Compoenets
+    final String DIV_NAME ="";
+    final String TAB_NAME ="";
+    final String TABPAGE_NAME ="";
+    final String STATIC_NAME ="";
+    final String UCODECOMBO ="";
+    final String UCALDAY_NAME ="";
+    final String UCALDAYFROMTO_NAME ="";
+    final String UCOMBO_NAME ="";
+    final String EDIT_NAME ="";
+    final String BUTTON_NAME ="";
+    final String GRID_NAME ="";
+
+    //tab로드
+    final String PRELOAD ="aadPreloadList";
+
+    //script영역
+    final String SCRIPTS ="registerScript";
+
+    // Regist UI Components Event
+    final String COMP_Event = "this.on_initEvent";
+
+
+
 }
--- src/main/java/convertJS.java
+++ src/main/java/convertJS.java
@@ -1,19 +1,40 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class convertJS {
     public static void main(String[] args) throws IOException
     {
         System.out.println(System.getProperty("user.dir"));
+
         try(FileReader fileReader = new FileReader("/Users/gaebabja/IdeaProjects/Mydev/src/main/resources/JS/test.txt"))
         {
+            List<String> Data= makeData(fileReader);
 
+            fileReader.close();
 
-            int ch;
-            while((ch=fileReader.read())!=-1)
+            List<String> output = new ArrayList<>();
+
+            //TODO Converting JS TO XFDL
+
+            File file = new File("/Users/gaebabja/IdeaProjects/Mydev/src/main/resources/XFDL/result.xfdl.js");
+            if(!file.exists())
             {
-                System.out.print((char) ch);
-
+                file.createNewFile();
             }
+
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            String strCode ="";
+            for(int i=0;i< output.size();i++)
+            {
+                strCode+= output.get(i);
+            }
+            bufferedWriter.write(strCode);
+
+            bufferedWriter.close();
+            fileWriter.close();
         }
         catch(Exception e)
         {
@@ -23,6 +44,29 @@ public class convertJS {
 
         }
 
+    }
+
+
+
+    public static List<String> makeData(FileReader fileReader) throws IOException
+    {
+        List<String> lines = new ArrayList<>();
+        int ch=0;
+        String stmt="";
+
+        while((ch=fileReader.read())!=-1)
+        {
+            if(ch!=13)
+            {
+                stmt+=(char)ch;
+            }
+            else
+            {
+                lines.add(stmt);
+                stmt="";
+            }
+        }
+        return lines;
     }
 
 
